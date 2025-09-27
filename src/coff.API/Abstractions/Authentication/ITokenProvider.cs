5c0f410209af4d30ac39e4d2e2eb0930af819858
kattcraftt <nishimiya9@yahoo.com>
2025-09-27 17:12:04 +0800

    add: abstractions for auth

---
+++ src/coff.API/Abstractions/Authentication/ITokenProvider.cs
@@ -0,0 +1,8 @@
+﻿using coff.API.SharedKernel.Domain.Users;
+
+namespace coff.API.Abstractions.Authentication;
+
+public interface ITokenProvider
+{
+    string Create(User user);
+}
---
+++ src/coff.API/Abstractions/Authentication/IUserContext.cs
@@ -0,0 +1,6 @@
+﻿namespace coff.API.Abstractions.Authentication;
+
+public interface IUserContext
+{
+    Guid UserId { get; }
+}
