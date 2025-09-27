f3a5acf7c80a5c3fd6f947947abb217a07feb421
kattcraftt <nishimiya9@yahoo.com>
2025-09-27 17:13:53 +0800

    add: abstraction for database

---
+++ src/coff.API/Abstractions/Data/IApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using coff.API.SharedKernel.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace coff.API.Abstractions.Data;
+
+public interface IApplicationDbContext
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
