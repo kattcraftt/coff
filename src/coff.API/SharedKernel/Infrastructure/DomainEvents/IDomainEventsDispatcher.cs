6cc604f4508bf7f198dd547eaf1849660b3c4c21
kattcraftt <nishimiya9@yahoo.com>
2025-09-27 17:16:08 +0800

    add: domain events dispatcher

---
+++ src/coff.API/SharedKernel/Infrastructure/DomainEvents/IDomainEventsDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using coff.API.SharedKernel;
+
+namespace coff.API.SharedKernel.Infrastructure.DomainEvents;
+
+public interface IDomainEventsDispatcher
+{
+    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+}
