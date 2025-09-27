bac8166a6e608293a48e3eca31b033e257059e40
kattcraftt <nishimiya9@yahoo.com>
2025-09-27 17:16:51 +0800

    add: database

---
+++ src/coff.API/SharedKernel/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,72 @@
+﻿using coff.API.Abstractions.Data;
+using coff.API.SharedKernel.Domain.Users;
+using coff.API.SharedKernel.Infrastructure.DomainEvents;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace coff.API.SharedKernel.Infrastructure.Database;
+
+public sealed class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options,
+    IDomainEventsDispatcher domainEventsDispatcher)
+    : IdentityDbContext<User, IdentityRole<Guid>, Guid>(options), IApplicationDbContext
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        #region Table configurations
+
+        builder.Entity<User>(e => e.ToTable("Users"));
+        builder.Entity<IdentityRole>(e => e.ToTable("Roles"));
+        builder.Entity<IdentityUserRole<string>>(e => e.ToTable("UserRoles"));
+        builder.Entity<IdentityUserClaim<string>>(e => e.ToTable("UserClaims"));
+        builder.Entity<IdentityUserLogin<string>>(e => e.ToTable("UserLogins"));
+        builder.Entity<IdentityUserToken<string>>(e => e.ToTable("UserTokens"));
+        builder.Entity<IdentityRoleClaim<string>>(e => e.ToTable("RoleClaims"));
+        
+        #endregion
+        
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        builder.HasDefaultSchema(Schemas.Default);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // When should you publish domain events?
+        //
+        // 1. BEFORE calling SaveChangesAsync
+        //     - domain events are part of the same transaction
+        //     - immediate consistency
+        // 2. AFTER calling SaveChangesAsync
+        //     - domain events are a separate transaction
+        //     - eventual consistency
+        //     - handlers can fail
+
+        int result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                List<IDomainEvent> domainEvents = entity.DomainEvents;
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        await domainEventsDispatcher.DispatchAsync(domainEvents);
+    }
+}
---
+++ src/coff.API/SharedKernel/Infrastructure/Database/Schemas.cs
@@ -0,0 +1,6 @@
+﻿namespace coff.API.SharedKernel.Infrastructure.Database;
+
+internal static class Schemas
+{
+    public const string Default = "public";
+}
