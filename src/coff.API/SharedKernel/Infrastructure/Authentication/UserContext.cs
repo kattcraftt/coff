9dd001e34e51529d015ac583735e697690ee7946
kattcraftt <nishimiya9@yahoo.com>
2025-09-27 17:18:58 +0800

    add: auth token provider

---
+++ src/coff.API/SharedKernel/Infrastructure/Authentication/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using coff.API.Abstractions.Authentication;
+using coff.API.SharedKernel.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace coff.API.SharedKernel.Infrastructure.Authentication;
+
+internal sealed class UserContext : IUserContext
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UserContext(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid UserId =>
+        _httpContextAccessor
+            .HttpContext?
+            .User
+            .GetUserId() ??
+        throw new ApplicationException("User context is unavailable");
+}
