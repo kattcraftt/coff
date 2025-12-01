6cc73f46e1746d7ce2f5fc744685dc6c60e39e68
kattcraftt <nishimiya9@yahoo.com>
2025-12-02 04:02:44 +0800

    update: add avatar image property on user

--- src/coff.API/Features/Users/Queries/Get/GetUsersQuery.cs
+++
@@ -1,5 +0,0 @@
-using coff.API.Abstractions.Messaging;
-
-namespace coff.API.Features.Users.Queries.Get;
-
-public sealed record GetUsersQuery(Guid userId) : IQuery<List<UserResponse>>;
---
+++ src/coff.API/SharedKernel/Infrastructure/Database/Migrations/20251201094133_AddUserProfileImage.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace coff.API.SharedKernel.Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class AddUserProfileImage : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "profile_image_url",
+            schema: "public",
+            table: "Users",
+            type: "text",
+            nullable: true);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "profile_image_url",
+            schema: "public",
+            table: "Users");
+    }
+}
+
