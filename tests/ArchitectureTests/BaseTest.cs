a69a046c8bfeb5e43a53c58200155e5f7cc8cab7
kattcraftt <nishimiya9@yahoo.com>
2025-12-03 17:42:33 +0800

    add: arch api tests

---
+++ tests/ArchitectureTests/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using coff.API.Abstractions.Messaging;
+using coff.API.SharedKernel.Domain.Users;
+using coff.API.SharedKernel.Infrastructure.Database;
+using coff.API;
+
+namespace ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+}
